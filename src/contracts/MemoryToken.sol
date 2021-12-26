813fe2eb1144a39d553888298fb2e8715a7a8d49
stefanbpeterson <stefanbpeterson@gmail.com>
2021-12-25 20:50:53 -0700

    leared aboout open zeppelin and implemented inheritance from their ERC721 token code

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.5.0;
 
 import "./ERC721Full.sol";
 
-contract MemoryToken {
-  string public name = "Memory Token";
+contract MemoryToken is ERC721Full {
+  constructor() ERC721Full('Memory Token', 'MEMORY') public {
+    
+  }
 }
