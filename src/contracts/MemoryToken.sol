e24fe81c5b15c9fc5caa97303d1b616feb2ae35a
stefanbpeterson <stefanbpeterson@gmail.com>
2021-12-25 20:14:27 -0700

    created token contract file and tested deployment to ganache

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -2,7 +2,6 @@ pragma solidity ^0.5.0;
 
 import "./ERC721Full.sol";
 
-contract MemoryToken is ERC721Full {
-  // Code goes here...
-
+contract MemoryToken {
+  string public name = "Memory Token";
 }
